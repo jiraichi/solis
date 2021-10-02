683d5099ddfbe47588700866723b0efe753f8614
jiraichi <shansp@protonmail.ch>
2021-10-02 18:36:40 +0800

    feat: add get angles for api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Months } from '../interfaces/angle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  apiUrl = 'https://l1zxnfptyk.execute-api.ap-southeast-1.amazonaws.com/dev/solar';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAngles(lat, long): Observable<Months> {
+    const url = `${this.apiUrl}/angle?latitude=${lat}&longitude=${long}`;
+    return this.http.get<Months>(url);
+  }
+
 }
