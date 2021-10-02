e6f304104be6f0df03a0312edb8ef71ee24425a7
jiraichi <shansp@protonmail.ch>
2021-10-02 19:10:57 +0800

    feat: get coordinates from params

--- src/app/pages/location-details/location-details.page.ts
+++ src/app/pages/location-details/location-details.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
-import {Observable} from 'rxjs';
-import {Months} from '../../interfaces/angle';
+import { Observable } from 'rxjs';
+import { Months } from '../../interfaces/angle';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-location-details',
@@ -11,14 +12,25 @@ import {Months} from '../../interfaces/angle';
 export class LocationDetailsPage implements OnInit {
 
   currentSegment = 'statistics';
+  lat: string;
+  long: string;
   angles$: Observable<Months>;
 
   constructor(
-    private api: ApiService
+    private api: ApiService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this.angles$ = this.api.getAngles('64.2008', '149.4937');
+    this.route.paramMap.subscribe(params => {
+      this.lat = params.get('coordinates').split('-')[0];
+      this.long = params.get('coordinates').split('-')[1];
+    });
+
+    console.log('coordinates', this.lat, this.long);
+
+    this.angles$ = this.api.getAngles(this.lat, this.long);
+    this.angles$.subscribe(res => console.log(res));
   }
 
   segmentChanged(event) {
