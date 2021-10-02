c066ff351044ab4b6879979bd8e9053853f31c4c
jiraichi <shansp@protonmail.ch>
2021-10-02 18:36:11 +0800

    feat: add  angles interface

---
+++ src/app/interfaces/angle.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export interface Months {
+  JAN: Angle;
+  FEB: Angle;
+  MAR: Angle;
+  APR: Angle;
+  MAY: Angle;
+  JUN: Angle;
+  JUL: Angle;
+  AUG: Angle;
+  SEP: Angle;
+  OCT: Angle;
+  NOV: Angle;
+  DEC: Angle;
+}
+
+export interface Angle {
+  horizontal: number;
+  vertical: number;
+}
+
